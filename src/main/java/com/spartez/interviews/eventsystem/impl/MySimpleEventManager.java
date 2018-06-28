b2cedbf00802465cd98b0385aa292ce4deaead7f
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-06-28 17:28:49 +0600

    locks and no stream api

--- src/main/java/com/spartez/interviews/eventsystem/impl/MySimpleEventManager.java
+++ src/main/java/com/spartez/interviews/eventsystem/impl/MySimpleEventManager.java
@@ -1,8 +1,10 @@
 package com.spartez.interviews.eventsystem.impl;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
@@ -13,39 +15,50 @@ import com.spartez.interviews.eventsystem.EventManager;
 public class MySimpleEventManager implements EventManager {
 
     private final ReadWriteLock lock = new ReentrantReadWriteLock();
+    private final Lock r = lock.readLock();
+    private final Lock w = lock.writeLock();
+
     private final Map<String, EventListener> listeners = new HashMap<>();
 
     @Override
-    public void publishEvent(Event event) {
+    public void publishEvent(final Event event) {
 	final Class<?> eventClazz = event.getClass();
+	final Collection<EventListener> eventListenertsCollection;
 	try {
-	    lock.readLock().lock();
-	    listeners.values().stream()
-		    .filter(l -> l.getHandledEventClasses().length == 0
-			    || Arrays.asList(l.getHandledEventClasses()).contains(eventClazz))
-		    .forEach(l -> l.handleEvent(event));
+	    r.lock();
+	    eventListenertsCollection = listeners.values();
 	} finally {
-	    lock.readLock().unlock();
+	    r.unlock();
 	}
+	for (EventListener listener : eventListenertsCollection) {
+	    final Class[] handlingClasses = listener.getHandledEventClasses();
+	    if (handlingClasses.length == 0 || Arrays.asList(handlingClasses).contains(eventClazz))
+		listener.handleEvent(event);
+	}
+	//
+	// listeners.values().stream()
+	// .filter(l -> l.getHandledEventClasses().length == 0
+	// || Arrays.asList(l.getHandledEventClasses()).contains(eventClazz))
+	// .forEach(l -> l.handleEvent(event));
     }
 
     @Override
     public void registerListener(String listenerKey, EventListener listener) {
 	try {
-	    lock.writeLock().lock();
+	    w.lock();
 	    listeners.put(listenerKey, listener);
 	} finally {
-	    lock.writeLock().unlock();
+	    w.unlock();
 	}
     }
 
     @Override
     public void unregisterListener(String listenerKey) {
 	try {
-	    lock.writeLock().lock();
+	    w.lock();
 	    listeners.remove(listenerKey);
 	} finally {
-	    lock.writeLock().unlock();
+	    w.unlock();
 	}
     }
 
